f88b6193a6e637677c9837c2e84a1c898b35b8d6
Kavi <thekavisharma26@gmail.com>
2025-04-14 18:46:18 +0530

    auth changes

--- Backend/Aryan/src/routes/auth.js
+++ Backend/Aryan/src/routes/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const authRouter = express.Router();
+const bcrypt = require("bcrypt")
 const validateData = require("../utils/validations")
 authRouter.post('/signup' , async (req, res )=>{
     try{
         validateData(req);
+        const {firstName  , lastName , email , password} = req.body;
+        const passWordHash = await bcrypt.hash(password ,10 );
+        const user = new User({
+            firstName,
+            lastName,
+            email,
+            password : passWordHash,
+        });
+        await user.save()
+        res.send("User added successfully");
+
     }
     catch(error){
         res.json({
@@ -11,6 +23,9 @@ authRouter.post('/signup' , async (req, res )=>{
         })
     }
 
+})
+authRouter.post('/login' , async(req,res)=>{
+    
 })
 
 module.exports = authRouter;
